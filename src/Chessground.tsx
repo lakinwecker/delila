f06045f04a238f8583406288e28d7f17512b99c8
Lakin Wecker <lakin@structuredabstraction.com>
2017-03-15 07:31:11 -0600

    WIP - attempting to embed a chessground instance. Not working.

--- src/Chessground.tsx
+++ src/Chessground.tsx
@@ -1,16 +1,18 @@
 import { Node } from 'tweed'
 import { Renderer } from './types'
-//import { Chessground } from 'chessground'
+import { Chessground } from 'chessground'
+import { Api } from 'chessground/api'
 
 export class ChessgroundWidget implements Renderer {
   //private _id = "asdf798asdf"
+  private cg?: Api = undefined;
   constructor () {
   }
 
   render (): Node {
     let cg_node = document.createElement('div');
     cg_node.innerText = "Reader";
-    //let cg = Chessground(cg_node, {});
+    this.cg = Chessground(cg_node, {});
     return (
       <chessground>
         <div>{cg_node}</div>
