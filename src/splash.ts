5467763965021a9e762906648b5c929738daa5f2
Lakin Wecker <lakin@structuredabstraction.com>
2017-05-30 10:09:54 -0600

    forgot this file

---
+++ src/splash.ts
@@ -0,0 +1,82 @@
+// The splash screen.
+import { Component, h, ConnectParams, RenderParams } from 'kaiju'
+import { update } from 'immupdate'
+import { InitializeState, Remote, factory, initialize } from './server/initialization'
+
+import routeStore from './stores/route'
+import { navigate, Route } from './stores/route'
+import VERSION from './version'
+
+//-----------------------------------------------------------------------------------------
+export default function () {
+  return Component<Props, State>({
+    name: 'importpgn',
+    props: defaultProps(),
+    initState,
+    connect,
+    render
+  })
+}
+
+//-----------------------------------------------------------------------------------------
+interface Props {
+  remoteStore: Remote
+}
+
+//-----------------------------------------------------------------------------------------
+function defaultProps(): Props {
+  let remoteStore = factory()
+  console.log(remoteStore.store);
+  remoteStore.store.send(
+    initialize({version: VERSION})
+  );
+  return {
+    remoteStore: remoteStore
+  }
+}
+ 
+//-----------------------------------------------------------------------------------------
+interface State {
+  remoteState: InitializeState,
+}
+ 
+function initState() {
+  return {
+    remoteState: {
+      activity: "...",
+      progress: 0,
+      version: VERSION,
+      finished: false
+    },
+  }
+}
+
+//-----------------------------------------------------------------------------------------
+function connect({ on, props }: ConnectParams<Props, State>) {
+  on(props().remoteStore.store.state, (state, remoteState: InitializeState) => {
+    let newState = update(state, {remoteState: remoteState})
+    if (newState.remoteState.finished) {
+      routeStore.send(navigate(Route.Dashboard))
+    }
+    return newState
+  })
+}
+
+//-----------------------------------------------------------------------------------------
+function render({ state }: RenderParams<Props, State>) {
+  return h("splash", {}, [
+    h("centre", [
+      h("h1", [
+        h("c.d1", "D"),
+        h("c.e1", "e"),
+        h("c.l1", "l"),
+        h("c.i1", "i"),
+        h("c.l2", "l"),
+        h("c.a1", "a"),
+      ]),
+      h("activity", state.remoteState.activity),
+    ]),
+  ])
+}
+
+
