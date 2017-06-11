9118bd84b5186221cbe5bba8715bd1b4badc9c8c
Lakin Wecker <lakin@structuredabstraction.com>
2017-06-10 18:48:57 -0600

    Better TODO message related to the version number.

--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,4 @@
-// TODO: this and the package.json have to be manually kept in sync
+// NOTE: this and the package.json have to be manually kept in sync
+// TODO: figure out an automated way to do that. :/
 const VERSION = "0.1.0"
 export default VERSION;
